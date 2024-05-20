a1fbb5437b5062d898a74a681ca134fc1fdd8dcd
Eleonora Virych <virycele@fel.cvut.cz>
2024-05-20 21:35:30 +0200

    update nav bar

--- dbs/src/main/java/cs/cvut/fel/dbs/main/Navigation.java
+++ dbs/src/main/java/cs/cvut/fel/dbs/main/Navigation.java
@@ -1,6 +1,7 @@
 package cs.cvut.fel.dbs.main;
 
 import cs.cvut.fel.dbs.db.DatabaseConnection;
+import cs.cvut.fel.dbs.main.special.ViewSections;
 import cs.cvut.fel.dbs.main.tables.addresses.AddressesView;
 import cs.cvut.fel.dbs.main.tables.climate_types.ClimateTypesView;
 import cs.cvut.fel.dbs.main.tables.employees.EmployeesView;
@@ -11,78 +12,69 @@ import cs.cvut.fel.dbs.main.tables.researches.ResearchesView;
 import cs.cvut.fel.dbs.main.tables.scientists.ScientistsView;
 import cs.cvut.fel.dbs.main.tables.sections.SectionsView;
 import cs.cvut.fel.dbs.main.tables.soil_types.SoilTypesView;
-import javafx.event.ActionEvent;
 import org.apache.log4j.LogManager;
 import org.apache.log4j.Logger;
 
 public class Navigation {
     private static final Logger logger = LogManager.getLogger(Navigation.class);
     public void handleCrudPlantsButton() {
-        // Load CRUD scene
-        logger.info("Loading CRUD scene...");
+        logger.info("Loading Plants CRUD scene...");
         PlantsView.showPlantsRecordsList();
     }
     public void handleCrudInstancesButton() {
-        // Load CRUD scene
-        logger.info("Loading CRUD scene...");
+        logger.info("Loading Instances CRUD scene...");
         InstancesView.showInstancesRecordsList();
     }
     public void handleCrudAddressesButton() {
-        // Load CRUD scene
-        logger.info("Loading CRUD scene...");
+        logger.info("Loading Addresses CRUD scene...");
         AddressesView.showAddressesRecordsList();
     }
-
-    public void handleSelectButton() {
-        // Load SELECT scene
-        logger.info("Loading SELECT scene...");
-    }
-    public void exit(ActionEvent actionEvent) {
+    public void exit() {
+        logger.info("Exiting application...");
         DatabaseConnection.close();
         GUI.getStage().close();
     }
-    public void mainMenu(ActionEvent actionEvent) {
+    public void mainMenu() {
+        logger.info("Loading main menu...");
         GUI.showMainMenu();
     }
 
-    public void handleCrudClimateTypesButton(ActionEvent actionEvent) {
-        // Load CRUD scene
-        logger.info("Loading CRUD scene...");
+    public void handleCrudClimateTypesButton() {
+        logger.info("Loading Climate Types CRUD scene...");
         ClimateTypesView.showClimateTypesRecordsList();
     }
-    public void handleCrudSoilTypesButton(ActionEvent actionEvent) {
-        // Load CRUD scene
-        logger.info("Loading CRUD scene...");
+    public void handleCrudSoilTypesButton() {
+        logger.info("Loading Soil Types CRUD scene...");
         SoilTypesView.showSoilTypesRecordsList();
     }
 
-    public void handleCrudPersonsButton(ActionEvent actionEvent) {
-        // Load CRUD scene
-        logger.info("Loading CRUD scene...");
+    public void handleCrudPersonsButton() {
+        logger.info("Loading Persons CRUD scene...");
         PersonsView.showPersonsRecordsList();
     }
 
-    public void handleCrudSectionsButton(ActionEvent actionEvent) {
-        // Load CRUD scene
-        logger.info("Loading CRUD scene...");
+    public void handleCrudSectionsButton() {
+        logger.info("Loading Sections CRUD scene...");
         SectionsView.showSectionsRecordsList();
     }
 
-    public void handleCrudScientistsButton(ActionEvent actionEvent) {
-        // Load CRUD scene
-        logger.info("Loading CRUD scene...");
+    public void handleCrudScientistsButton() {
+        logger.info("Loading Scientists CRUD scene...");
         ScientistsView.showScientistsRecordsList();
     }
 
-    public void handleCrudResearchesButton(ActionEvent actionEvent) {
-        // Load CRUD scene
-        logger.info("Loading CRUD scene...");
+    public void handleCrudResearchesButton() {
+        logger.info("Loading Researches CRUD scene...");
         ResearchesView.showResearchesRecordsList();
     }
 
-    public void handleCrudEmployeesButton(ActionEvent actionEvent) {
-        // Load CRUD scene
-        logger.info("Loading CRUD scene...");
+    public void handleCrudEmployeesButton() {
+        logger.info("Loading Employees CRUD scene...");
         EmployeesView.showEmployeesRecordsList();
+    }
+
+    public void handleViewSectionsButton() {
+        logger.info("Loading Sections view scene...");
+        ViewSections.getSectionsViewPage();
     }
 }
